a33ae67e3f74714a27b54c2ad4f498ffaad35e47
Rafael Moreno <morenraf000@warren.k12.in.us>
2024-02-07 11:57:22 -0500

    assignment

---
+++ 01_Strings/05_Search Strings/Assignment.py
@@ -0,0 +1,10 @@
+text = input("Enter some text: ")
+
+result = text.find("the")
+
+if result == -1 :
+    print("the word the is in not in the string")
+else:
+    print("the word the is in the string")
+    print("it is located at", result)
+   
